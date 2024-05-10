c3a036b91949d6c1effb4e62a8767e1579412d99
AndreasRaaskov <s183901@student.dtu.dk>
2024-05-10 22:46:09 +0200

    Update ChatGPT to provede logits

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,8 +3,8 @@ import openai
 import dotenv
 
 
-#MODEL_ID = "gpt-3.5-turbo-0301"
-MODEL_ID = "gpt-4-0613"
+MODEL_ID = "gpt-3.5-turbo-0301"
+#MODEL_ID = "gpt-4-0613"
 
 keys = dotenv.dotenv_values("keys.env")
 openai.api_key = keys["OPENAI_API_KEY"]
@@ -13,10 +13,16 @@ openai.api_key = keys["OPENAI_API_KEY"]
 
 
 def GPT_answer(message: str) -> str:
-    return (
-        openai.ChatCompletion.create(
-            model=MODEL_ID, messages=[{"role": "user", "content": message}]
+    #gives the response to the message and the logprobs of the tokens
+    output=openai.chat.completions.create(
+            model=MODEL_ID, messages=[{"role": "user", "content": message}],logprobs=True
         )
-        .choices[0]
-        .message.content
-    )
+    tokens = []
+    logprobs = []
+    for item in output.choices[0].logprobs.content:
+        tokens.append(item.token)
+        logprobs.append(item.logprob)
+    return tokens,logprobs
+
+if __name__ == "__main__":
+    print(GPT_answer("What is the meaning of life"))
